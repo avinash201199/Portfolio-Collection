98261a34536b3a2ce648fa77302698330365f1b6
Avinash Singh <61057666+avinash201199@users.noreply.github.com>
2021-10-02 15:45:44 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const hamburgerButton = document.getElementById("hamburger")
-const navIcons = document.getElementById("nav-list")
-
-function toggleButton(){
-   navIcons.classList.toggle('show')
-}
-
-hamburgerButton.addEventListener('click', toggleButton)
