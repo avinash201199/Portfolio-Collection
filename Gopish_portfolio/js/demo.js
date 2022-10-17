3e4bd5e129854491b9e8a959839b93dc1704105a
Gopish Mahajan <gopishmahajan12@gmail.com>
2022-10-17 11:47:47 +0530

    Added personal portfolio

---
+++ Gopish_portfolio/js/demo.js
@@ -0,0 +1,28 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+// This can be used to set the Particles Effects. Check README for more details!
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
