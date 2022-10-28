aadf7151d260e494f9830894aa3c0cb7c38fa862
hikaaam <andrenuryana@gmail.com>
2022-10-28 23:41:07 +0700

    add ilyas's portfolio

---
+++ Ilyas_A_Yusuf/readme.md
@@ -0,0 +1,14 @@
+## [DEMO](https://hikaaam.github.io)
+
+---
+
+Run :
+
+1. `yarn install`
+1. `yarn dev`
+
+---
+
+Build :
+
+`yarn build`
---
+++ Ilyas_A_Yusuf/src/Components/index.tsx
@@ -0,0 +1,5 @@
+import NavBar from "./NavBar";
+import PortofolioCard from "./PortofolioCard";
+import TechCard from "./TechCard";
+
+export { NavBar, PortofolioCard, TechCard };
