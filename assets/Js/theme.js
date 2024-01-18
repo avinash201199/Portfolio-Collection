f2e483eb0ced7f7dad0658999567e7a3751152b6
Dhruva Bhat <dhruvabhat24@gmail.com>
2024-01-18 14:59:20 +0000

    rebase

--- assets/Js/theme.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('theme-switcher').addEventListener('click', function() {
-    if (document.body.getAttribute('data-theme') === 'dark') {
-        document.body.removeAttribute('data-theme');
-    } else {
-        document.body.setAttribute('data-theme', 'dark');
-    }
-});
